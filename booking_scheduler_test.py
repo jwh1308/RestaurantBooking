6f22d9852bc2e5f4a533895eb6df0563d18080ed
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-11 11:13:06 +0900

    [add] test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from booking_scheduler import BookingScheduler
 from schedule import Schedule, Customer
@@ -43,7 +43,14 @@ class BookingSchedulerTest(unittest.TestCase):
         self.assertEqual(expected, str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
-        pass
+        schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+        self.scheduler.add_schedule(schedule)
+
+        new_schedule = Schedule(ON_THE_HOUR + timedelta(hours=1), UNDER_CAPACITY, CUSTOMER)
+        self.scheduler.add_schedule(new_schedule)
+
+        self.assertTrue(self.scheduler.has_schedule(schedule))
+        self.assertTrue(self.scheduler.has_schedule(new_schedule))
 
     def test_예약완료시_SMS는_무조건_발송(self):
         pass
